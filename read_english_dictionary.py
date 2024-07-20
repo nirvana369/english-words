36752585659c6b5b3707087b486dde24132df308
#undefined <hoanglinh91@gmail.com>
2024-07-20 14:52:06 +0700

    Update read_english_dictionary.py
    Find an unpredictable word by adding and multiplying letters

--- read_english_dictionary.py
+++ read_english_dictionary.py
@@ -4,8 +4,44 @@ def load_words():
 
     return valid_words
 
+def plus_cal(x):
+    sum = 0
+    for y in range(0, len(x)):
+        sum += ord(x[y]) - 64
+    return sum
+
+def mul_cal(x):
+    mul = 1
+    for y in range(0, len(x)):
+        mul *= ord(x[y]) - 64
+    return mul
+
+def search(x, plus, mul):
+    p = plus_cal(x.upper())
+    m = mul_cal(x.upper())
+    if plus.get(p) != None and mul.get(m) != None:
+        arr = []
+        for i in plus[p]:
+            if i in mul[m]:
+                arr.append(i)
+        return arr
+    return ["not found"]
 
 if __name__ == '__main__':
     english_words = load_words()
-    # demo print
-    print('fate' in english_words)
+    plus = {}
+    mul = {}
+    for x in english_words:
+        p = plus_cal(x.upper())
+        m = mul_cal(x.upper())
+        if plus.get(p) != None:
+            plus[p].append(x)
+        else:
+            plus[p] = [x]
+
+        if mul.get(m) != None:
+            mul[m].append(x)
+        else:
+            mul[m] = [x]
+    ret = search('nvsoeur', plus, mul)
+    print(ret)
